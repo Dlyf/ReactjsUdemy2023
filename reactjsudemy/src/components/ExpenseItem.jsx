4b388bff511672c7d9b59a1e1bb5086f42f3dab8
Dlyf <domilung@gmail.com>
2023-05-18 21:16:07 +0200

    REACT-004 dynamic data & working with expressions in JSX

--- reactjsudemy/src/components/ExpenseItem.jsx
+++ reactjsudemy/src/components/ExpenseItem.jsx
@@ -1,11 +1,16 @@
 import './ExpenseItem.css';
 function ExpenseItem() {
+    // fake dynamic data
+    const expenseDate = new Date(2022, 5, 18);
+    const expenseTitle = 'CarInsurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-            <div>28 Mars 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2 className="expense-item h2">Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2 className="expense-item h2">{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
         )
