5884006121febdce87bc843349c0e5f159b2bcad
Dlyf <domilung@gmail.com>
2023-05-18 18:29:00 +0200

    REACT-003 Adding Basic CSS styling

--- reactjsudemy/src/components/ExpenseItem.jsx
+++ reactjsudemy/src/components/ExpenseItem.jsx
@@ -1,10 +1,11 @@
+import './ExpenseItem.css';
 function ExpenseItem() {
     return (
-        <div>
+        <div className="expense-item">
             <div>28 Mars 2021</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$294.67</div>
+            <div className="expense-item__description">
+                <h2 className="expense-item h2">Car Insurance</h2>
+                <div className="expense-item__price">$294.67</div>
             </div>
         </div>
         )
