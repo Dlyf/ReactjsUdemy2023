4c24d20303accec2e8209ddda85c791d8d33803f
Dlyf <domilung@gmail.com>
2023-05-20 12:43:57 +0200

    REACT-005 Passing data via props

--- reactjsudemy/src/App.jsx
+++ reactjsudemy/src/App.jsx
@@ -1,9 +1,56 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28)
+
+    },
+    {
+      id: 'e2',
+      title: 'CarB insurance',
+      amount: 291.67,
+      date: new Date(2021, 2, 28)
+
+    },
+    {
+      id: 'e3',
+      title: 'CarC insurance',
+      amount: 220,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e4',
+      title: 'CarD insurance',
+      amount: 270,
+      date: new Date(2021, 2, 28)
+    }
+  ]
   return (
     <>
       <h1>Vite + React</h1>
-      <ExpenseItem />
+      <ExpenseItem
+       title={expenses[0].title}
+       amount={expenses[0].amount}
+       date={expenses[0].date}
+      />
+      <ExpenseItem
+       title={expenses[1].title}
+       amount={expenses[1].amount}
+       date={expenses[1].date}
+      />
+      <ExpenseItem
+       title={expenses[2].title}
+       amount={expenses[2].amount}
+       date={expenses[2].date}
+      />
+      <ExpenseItem
+       title={expenses[3].title}
+       amount={expenses[3].amount}
+       date={expenses[3].date}
+      />
     </>
   )
 }
--- reactjsudemy/src/components/ExpenseItem.jsx
+++ reactjsudemy/src/components/ExpenseItem.jsx
@@ -1,19 +1,18 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
+function ExpenseItem(props) {
     // fake dynamic data
-    const expenseDate = new Date(2022, 5, 18);
-    const expenseTitle = 'CarInsurance';
-    const expenseAmount = 294.67;
-
+    // const expenseDate = new Date(2022, 5, 18);
+    // const expenseTitle = 'CarInsurance';
+    // const expenseAmount = 294.67;
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2 className="expense-item h2">{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2 className="expense-item h2">{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
-        )
+    );
 }
 
 export default ExpenseItem;
