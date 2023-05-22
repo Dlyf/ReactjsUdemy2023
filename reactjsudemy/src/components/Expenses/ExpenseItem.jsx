2b8e8e88dc492519295272a89844b435da25f8d0
Dlyf <domilung@gmail.com>
2023-05-22 22:37:23 +0200

    REACT-0010 Events & working with Event Handlers example

--- reactjsudemy/src/components/Expenses/ExpenseItem.jsx
+++ reactjsudemy/src/components/Expenses/ExpenseItem.jsx
@@ -2,6 +2,9 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 function ExpenseItem(props) {
+    const clickHandler = () => {
+        console.log("Clicked !!!!");
+    }
     return (
         <Card className="expense-item">
             {/* self-closing element */}
@@ -10,6 +13,7 @@ function ExpenseItem(props) {
                 <h2 className="expense-item h2">{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
