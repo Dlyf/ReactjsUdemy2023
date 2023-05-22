d9f0d761ff25380275d4f3c58c9cd43506828b82
Dlyf <domilung@gmail.com>
2023-05-22 23:03:48 +0200

    REACT-0011 Working with state - a key concept of react

--- reactjsudemy/src/components/Expenses/ExpenseItem.jsx
+++ reactjsudemy/src/components/Expenses/ExpenseItem.jsx
@@ -1,8 +1,19 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 function ExpenseItem(props) {
+    // array destructuring : (we import useState from react) a value + a function who change a value to modify, we define in our
+    // useState a props.title by default. 
+    const [title, setTitle] = useState(props.title);
+
+    // let title = props.title;
+    // ClickHandler is a function who trigger the click event
     const clickHandler = () => {
+        // We call here our function of usestate to modify a value
+        setTitle("Updated!");
+
         console.log("Clicked !!!!");
     }
     return (
@@ -10,9 +21,10 @@ function ExpenseItem(props) {
             {/* self-closing element */}
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2 className="expense-item h2">{props.title}</h2>
+                <h2 className="expense-item h2">{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            {/* we call the function clickHandler without parenthesis */}
             <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
