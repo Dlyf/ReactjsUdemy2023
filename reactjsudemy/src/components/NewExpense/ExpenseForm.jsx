a884f1fdf85ae26f5bc6bf11adf0947128b1d7d7
Dlyf <domilung@gmail.com>
2023-05-28 10:25:05 +0200

    REACT-0013 how to listen to User Input

--- reactjsudemy/src/components/NewExpense/ExpenseForm.jsx
+++ reactjsudemy/src/components/NewExpense/ExpenseForm.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+    // document.getElementById('').addEventListener('click', (event) => {
+
+    // })
+    // question : how can we get that value ? Which possibilities do we have here ?
+    const titleChangeHandler = (event) => {
+        console.log("toto", event.target.value);
+    }
+
     return <form>
         <div className="new-expense__controls">
             <div className="new-expense__controls">
                 <label>Title</label>
-                <input type="text" />
+                <input type="text" onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__controls">
                 <label>Amount</label>
