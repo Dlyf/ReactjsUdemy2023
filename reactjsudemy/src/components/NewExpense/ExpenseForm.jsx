00465d33ccfd15bb6b3db27efb145d29c1a4056c
Dlyf <domilung@gmail.com>
2023-05-28 10:46:53 +0200

    REACT-0015 an other method with useState

--- reactjsudemy/src/components/NewExpense/ExpenseForm.jsx
+++ reactjsudemy/src/components/NewExpense/ExpenseForm.jsx
@@ -4,28 +4,47 @@ import './ExpenseForm.css'
 
 const ExpenseForm = () => {
 
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    // const [enteredTitle, setEnteredTitle] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
+    // method two: 
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    })
+
+    const titleChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value,
+        })
+    }
     // document.getElementById('').addEventListener('click', (event) => {
 
     // })
     // question : how can we get that value ? Which possibilities do we have here ?
     // What do we want to do  with that value ? What's the goal now ? We want to make sure that
     // we store it somewhere so that when later when the form is submitted, we can use that value.
-    const titleChangeHandler = (event) => {
-        // console.log("toto", event.target.value);
-        setEnteredTitle(event.target.value);
-    }
+    // const titleChangeHandler = (event) => {
+    //     // console.log("toto", event.target.value);
+    //     setEnteredTitle(event.target.value);
+    // }
 
     // How can you manage more than one State ? We can have multiple States, multiple States slices
     // or state pieces per component
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value,
+        })
     }
 
     const dateChangeHandle = (event) => {
-        setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value,
+        })
     }
 
     return <form>
