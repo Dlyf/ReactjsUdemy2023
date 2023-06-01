5dba5dd586d96a9c4a34c242ba559b722ed48a5e
Dlyf <domilung@gmail.com>
2023-06-01 23:35:46 +0200

    REACT-0021 Using Stateful List with ...prevExpenses for dynamic list

--- reactjsudemy/src/App.jsx
+++ reactjsudemy/src/App.jsx
@@ -1,40 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expense from "./components/Expenses/Expense";
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Car insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28)
+
+  },
+  {
+    id: 'e2',
+    title: 'CarB insurance',
+    amount: 291.67,
+    date: new Date(2021, 2, 28)
+
+  },
+  {
+    id: 'e3',
+    title: 'CarC insurance',
+    amount: 220,
+    date: new Date(2021, 2, 28)
+  },
+  {
+    id: 'e4',
+    title: 'CarD insurance',
+    amount: 270,
+    date: new Date(2021, 2, 28)
+  },
+];
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Car insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28)
-
-    },
-    {
-      id: 'e2',
-      title: 'CarB insurance',
-      amount: 291.67,
-      date: new Date(2021, 2, 28)
-
-    },
-    {
-      id: 'e3',
-      title: 'CarC insurance',
-      amount: 220,
-      date: new Date(2021, 2, 28)
-    },
-    {
-      id: 'e4',
-      title: 'CarD insurance',
-      amount: 270,
-      date: new Date(2021, 2, 28)
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
   }
   return (
     <>
